b9356791f496747207f8120efe3dc832dbd55356
idealogiks <bernard.glds@gmail.com>
2026-07-17 18:42:03 +0200

    feat: ajout méthodes pour avoir et devis

--- documents/Avoir.js
+++ documents/Avoir.js
@@ -1,5 +1,20 @@
 import Documents from "./Documents.js";
 
 export default class Avoir extends Documents {
-    
+    getTitre() {
+        return `<header>Devis n- ${this.data.numero} \n Client : ${this.data.client}</header>`
+    }
+
+    afficherArticles() {
+        return "<ul>" + this.data.lignes.map(ligne =>`<li>${ligne.description} : ${ligne.prix} €</li>`) + "</ul>";
+    }
+
+    calculerTotal() {
+        let total = 0;
+        for (let ligne of this.data.lignes) {
+            total = total + ligne.prix;
+        }
+                
+        return `<p>Total remboursé : ${total} €</p>`;
+    }
 }
